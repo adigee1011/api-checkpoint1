7f5962e0aade8c36abb520b0872dd9ee0466f093
adigee1011 <adigee1011@gmail.com>
2022-05-12 15:24:32 -0400

    commit

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 console.log("loading main.js file")
 
 
-//make a reuqest to //https.//randomuser.me/api
-// and that service will send a response bac
-// and we want to do something with that response
-//may be just print it on the console
-
-
-// we really have 3 'programs' 
-// sending the html and javascript files to the browser
-// chrome displaying the html as a nice webpage and executing the main.js file
-// randomwuser.me's servers send us the response {json object}
-
-
-
-//some code here...
-console.log("before fetch")
-//let promise = fetch("https://randomuser.me/api") //10 seconds to respond 
 
 let button = document.getElementById("submit")
 button.addEventListener("click", function(){
@@ -26,7 +10,19 @@ button.addEventListener("click", function(){
     return response.json()
     
 }).then(function(json){
-    console.log("the json payload is", json.results[0].name)
+    console.log("My name is", json.results[0].name["first"])
+})
+})
+
+let button1 = document.getElementById("submit1")
+button1.addEventListener("click", function(){
+    fetch("https://ip-fast.com/api/ip/?format=json&location=True").then(function(response){
+    //do this when the results come back
+    console.log("processing fetch results")
+    return response.json()
+    
+}).then(function(json){
+    console.log("My IP address is", json.ip)
 })
 })
 
